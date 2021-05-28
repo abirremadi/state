27a394b05735da4fff14d83197ef84fd97f8899c
abirremadi <abirremadi4@gmail.com>
2021-05-29 00:11:07 +0100

    adding state files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+        FullName : 'Abir Remadi',
+        Profession: 'Mechatronics Engineer & a Web Developer',
+        Bio: 'Hello it s me Abir , Welcome to my Profile , Iam 26 years old , iam a mechatronics engineer and a web developer student.' ,
+        SrcImg : './profileimg.jpg',
+        Show: false,
+        button:'Show Profile'
+        
+    }
+  }
+  isToggle = () => {
+    this.setState(state => ({
+      Show : !this.state.Show
+    }))
+  }
+
+  render() {
+    const Show = this.state.Show;
+    return (
+  <div className='Profile'>
+      { Show && (
+      <>
+        <h1> {this.state.FullName} </h1>
+        <h3> {this.state.Profession} </h3>
+        <p className='bio'> {this.state.Bio} </p>
+        <img src={this.state.SrcImg} alt='profileimge'></img>
+      </>
+      )}
+      
+      <br/> <br/>
+      <button onClick={this.isToggle}>{Show?'Hide Profile':this.state.button}</button>
+  </div>
+    )
+  }
 }
 
-export default App;
+export default App
